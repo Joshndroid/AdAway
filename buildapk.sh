df62bf405828f5c9356cb68f0f774ce14b845e5b
Joshndroid <fmxjoshiewah@gmail.com>
2018-03-07 13:58:46 +1000

    Fixup buildapk.sh

--- buildapk.sh
+++ buildapk.sh
@@ -5,11 +5,11 @@
 #Build APK
 #cd ..
 #./gradlew renameExecutables
-./gradle --stop
+./gradlew --stop
 ./gradlew clean build
 #Now to sign the apks
-java -jar ~/uber-apk-signer-0.8.3.jar --apks /media/joshndroid/scratch/AdAway/AdAway/build/outputs/apk
+java -jar ~/uber-apk-signer-0.8.3.jar --apks /media/joshndroid/scratch/AdAway/AdAway/build/outputs/apk/release
 #Lets make the apk easier to integrate with a rom as a prebuilt
-cd /media/joshndroid/scratch/AdAway/AdAway/build/outputs/apk/
+cd /media/joshndroid/scratch/AdAway/AdAway/build/outputs/apk/release
 cp AdAway-release-aligned-debugSigned.apk AdAway.apk
 exit
